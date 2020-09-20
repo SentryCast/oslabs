d5f7e77615295b90d94dfc04eba873cd964aca88
user <user@localhost.localdomain>
2020-09-21 02:55:35 +0300

    minor fix

--- log.sh
+++ log.sh
@@ -5,14 +5,14 @@ inputfile="/var/log/anaconda/X.log"
 if [[ -f "/var/log/anaconda/X.log" && -r "/var/log/anaconda/X.log" ]]; then
 	while IFS= read -r line
 	do
-		if [[ "$line" == *"(WW)"* ]]; then
+		if [[ "$line" == *"(WW)"* && "$line" != *"(WW) warning, (EE) error, (NI) not implemented, (??) unknown."* ]]; then
 			echo -e ${line//"(WW)"/"\e[1;33mWarning:\e[0m"}
 		fi
 	done < "$inputfile"
 
 	while IFS= read -r line
 	do
-		if [[ "$line" == *"(II)"* ]]; then
+		if [[ "$line" == *"(II)"* && "$line" != *"(WW) warning, (EE) error, (NI) not implemented, (??) unknown."* ]]; then
 			echo -e ${line//"(II)"/"\e[1;36mInformation:\e[0m"}
 		fi
 	done < "$inputfile"
--- searchgrep.sh
+++ searchgrep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -e $1 ]; then
-	grep -R "$2" "$1"
+if [ -d $1 ]; then
+	grep -Rs "$2" "$1"
 else
 	echo Error occurred: can not read directory.
 	exit 1
