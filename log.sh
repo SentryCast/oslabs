eae266b442e4db95e24e25209b1d615af5287d77
user <user@localhost.localdomain>
2020-09-21 01:39:14 +0300

    minor fix

--- log.sh
+++ log.sh
@@ -13,7 +13,7 @@ if [[ -f "/var/log/anaconda/X.log" && -r "/var/log/anaconda/X.log" ]]; then
 	while IFS= read -r line
 	do
 		if [[ "$line" == *"(II)"* ]]; then
-			echo -e ${line//"(II)"/"\e[1;36mInfromation:\e[0m"}
+			echo -e ${line//"(II)"/"\e[1;36mInformation:\e[0m"}
 		fi
 	done < "$inputfile"
 else
