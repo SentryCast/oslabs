39ebe94fd8f8e680cc4edbbb05eaac19fc4d8725
user <user@localhost.localdomain>
2020-09-21 03:53:00 +0300

    minor fix

--- reverse.sh
+++ reverse.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-# ./main reverse data output
 
+if [[ -e $2 ]]; then
+	echo All files are found
+else
+	touch "$2"
 
-
-if [[ -f $2 && -r $2 ]]; then
-	if [ "$2" != "$3" ]; then
-		rev $2 | tac > $3
+	if [[ -f $2 && -r $2 ]]; then
+		if [ "$1" != "$2" ]; then
+			rev $1 | tac > $2
+		else
+			 echo Error occurred: datafile and outputfile should be different files.
+			 echo Usage: ./main reverse datafile outputfile
+			 exit 1
+		fi
 	else
-		echo Error occurred: datafile and outputfile should be different files.
+		echo Error occured: datafile can not be opened or does not exist.
 		echo Usage: ./main reverse datafile outputfile
 		exit 1
 	fi
-else
-	echo Error occured: datafile can not be opened or does not exist.
-	echo Usage: ./main reverse datafile outputfile
-	exit 1
 fi
